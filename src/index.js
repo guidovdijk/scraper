609c933ec1656dc7642ecf7fe139a5d62cac1e53
guidovdijk <32161849+guidovdijk@users.noreply.github.com>
2018-12-05 22:28:58 +0100

    Added better data structure + bug fixing image src

--- src/index.js
+++ src/index.js
@@ -16,9 +16,7 @@
   - Check of het bedrag een euro teken heeft.
   - Sommige bedragen (zoals die van alternate) hebben een '*' aan het einde
 
-  format this.config:
-  - Heel veel herhaling met selectors voor dezelfde websites.
-  - Alle winkjels in aparte bestanden zetten en importeren.
+  - Zed website titel bij de producten.
 
   laadtijd:
   - Verzin wat voor de C.O.R.S error
@@ -27,6 +25,7 @@
 */
 const rp = require('request-promise');
 const $ = require('cheerio');
+const dataBundle = require('./config.json')
 
 class Scraper {
   constructor(){
@@ -34,9 +33,9 @@ class Scraper {
       template: {
         _string: `
         <div class="product">
-          <img src="https:{{image}}"/>
+          <img src="{{srcprefix}}{{image}}"/>
           <h1>{{title}}</h1>
-          <a href="{{link}}">Go to product</a>
+          <a href="{{link}}" target="_blank">Go to product</a>
           <p class="price price--old">{{oldprice}}</p>
           <p class="price price--new">{{newprice}}</p>
         </div>`,
@@ -49,206 +48,21 @@ class Scraper {
           image: 'image',
         }
       },
-      data: [
-        /* mediamarkt */
-        {
-          url: 'https://www.mediamarkt.nl/nl/product/_logitech-g-g513-carbon-tactile-1563720.html',
-          title: 'h1',
-          image: '#product-sidebar .zoom img',
-          price: {
-            old: '.sidebar-form .price-old',
-            new: '.sidebar-form .price.big',
-          },
-        },
-        {
-          url: 'https://www.mediamarkt.nl/nl/product/_logitech-g-g502-hero-1592614.html',
-          title: 'h1',
-          image: '#product-sidebar .zoom img',
-          price: {
-            old: '.sidebar-form .price-old',
-            new: '.sidebar-form .price.big',
-          },
-        },
-        {
-          url: 'https://www.mediamarkt.nl/nl/product/_logitech-g-g840-xl-1537518.html',
-          title: 'h1',
-          image: '#product-sidebar .zoom img',
-          price: {
-            old: '.sidebar-form .price-old',
-            new: '.sidebar-form .price.big',
-          },
-        },
-        {
-          url: 'https://www.mediamarkt.nl/nl/product/_hyperx-cloud-alpha-gaming-headset-1558266.html?ga_query=cloud+alpha',
-          title: 'h1',
-          image: '#product-sidebar .zoom img',
-          price: {
-            old: '.sidebar-form .price-old',
-            new: '.sidebar-form .price.big',
-          },
-        },
-        {
-          url: 'https://www.mediamarkt.nl/nl/product/_samsung-860-evo-250-gb-1557211.html',
-          title: 'h1',
-          image: '#product-sidebar .zoom img',
-          price: {
-            old: '.sidebar-form .price-old',
-            new: '.sidebar-form .price.big',
-          },
-        },
-        /* coolblue */
-        {
-          url: 'https://www.coolblue.nl/product/808943/logitech-g513-tactile-mechanical-gaming-keyboard-qwerty.html',
-          title: 'h1 .js-product-name',
-          image: '.product-media-gallery__item.swiper-slide-active img',
-          price: {
-            old: '.js-sticky-header-end .sales-price__former',
-            new: '.js-sticky-header-end .sales-price__current',
-          },
-        },
-        {
-          url: 'https://www.coolblue.nl/product/820146/logitech-g502-hero-high-performance-gaming-mouse.html',
-          title: 'h1 .js-product-name',
-          image: '.product-media-gallery__item.swiper-slide-active img',
-          price: {
-            old: '.js-sticky-header-end .sales-price__former',
-            new: '.js-sticky-header-end .sales-price__current',
-          },
-        },
-        {
-          url: 'https://www.coolblue.nl/product/792520/logitech-g840-xl-gaming-mouse-pad.html',
-          title: 'h1 .js-product-name',
-          image: '.product-media-gallery__item.swiper-slide-active img',
-          price: {
-            old: '.js-sticky-header-end .sales-price__former',
-            new: '.js-sticky-header-end .sales-price__current',
-          },
-        },
-        {
-          url: 'https://www.coolblue.nl/product/802659/hyperx-cloud-alpha-pro-gaming-headset.html',
-          title: 'h1 .js-product-name',
-          image: '.product-media-gallery__item.swiper-slide-active img',
-          price: {
-            old: '.js-sticky-header-end .sales-price__former',
-            new: '.js-sticky-header-end .sales-price__current',
-          },
-        },
-        {
-          url: 'https://www.coolblue.nl/product/801969/samsung-860-evo-250gb-2-5-inch.html',
-          title: 'h1 .js-product-name',
-          image: '.product-media-gallery__item.swiper-slide-active img',
-          price: {
-            old: '.js-sticky-header-end .sales-price__former',
-            new: '.js-sticky-header-end .sales-price__current',
-          },
-        },
-        /* bol.com */
-        {
-          url: 'https://www.bol.com/nl/p/logitech-g513-tactile-mechanisch-gaming-toetsenbord-qwerty/9200000090809252/?suggestionType=suggestedsearch&bltgh=owsPjm5ybw0WvPVSuIlL3A.1.2.ProductTitle',
-          title: 'h1',
-          image: '.product-image-content img',
-          price: {
-            old: '.ab-discount del',
-            new: '.buy-block__prices .promo-price',
-          },
-        },
-        {
-          url: 'https://www.bol.com/nl/p/logitech-g502-hero-rgb-gaming-muis-pc/9200000098065282/?suggestionType=typedsearch&bltgh=pDC7vhsxCA8jWM4172lGoQ.1.3.ProductImage',
-          title: 'h1',
-          image: '.product-image-content img',
-          price: {
-            old: '.ab-discount del',
-            new: '.buy-block__prices .promo-price',
-          },
-        },
-        {
-          url: 'https://www.bol.com/nl/p/logitech-g840-xl-gaming-muismat/9200000081859830/?suggestionType=featured_product&suggestedFor=g84&originalSearchContext=media_all&originalSection=main',
-          title: 'h1',
-          image: '.product-image-content img',
-          price: {
-            old: '.ab-discount del',
-            new: '.buy-block__prices .promo-price',
-          },
-        },
-        {
-          url: 'https://www.bol.com/nl/p/hyperx-cloud-alpha-gaming-headset-ps4-xbox-one-nintendo-switch-windows-mobile-black/9200000095625954/?suggestionType=featured_product&suggestedFor=cloud%20al&originalSearchContext=media_all&originalSection=main',
-          title: 'h1',
-          image: '.product-image-content img',
-          price: {
-            old: '.ab-discount del',
-            new: '.buy-block__prices .promo-price',
-          },
-        },
-        {
-          url: 'https://www.bol.com/nl/p/samsung-860-evo-250gb-ssd/9200000087796720/',
-          title: 'h1',
-          image: '.product-image-content img',
-          price: {
-            old: '.ab-discount del',
-            new: '.buy-block__prices .promo-price',
-          },
-        },
-        /* alternate */
-        {
-          url: 'https://www.alternate.nl/Logitech/G513-CARBON-RGB-Tactile-Mechanical-Gaming-Keyboard/html/product/1433387?event=search',
-          title: 'h1 span',
-          image: '#bigPic img',
-          price: {
-            old: '.productShort .msrp span',
-            new: '.productShort .price span',
-          },
-        },
-        {
-          url: 'https://www.alternate.nl/Logitech/G502-HERO-High-Performance-Gaming-Mouse/html/product/1479074?event=search',
-          title: 'h1 span',
-          image: '#bigPic img',
-          price: {
-            old: '.productShort .msrp span',
-            new: '.productShort .price span',
-          },
-        },
-        {
-          url: 'https://www.alternate.nl/Logitech/G840-XL-Gaming-Mouse-pad/html/product/1380580?event=search',
-          title: 'h1 span',
-          image: '#bigPic img',
-          price: {
-            old: '.productShort .msrp span',
-            new: '.productShort .price span',
-          },
-        },
-        {
-          url: 'https://www.alternate.nl/HyperX/Cloud-Alpha-Pro-Gaming-Headset/html/product/1432269?event=search',
-          title: 'h1 span',
-          image: '#bigPic img',
-          price: {
-            old: '.productShort .msrp span',
-            new: '.productShort .price span',
-          },
-        },
-        {
-          url: 'https://www.alternate.nl/Samsung/860-EVO-250-GB-SSD/html/product/1405516?lk=15405',
-          title: 'h1 span',
-          image: '#bigPic img',
-          price: {
-            old: '.productShort .msrp span',
-            new: '.productShort .price span',
-          },
-        },
-      ],
+      data: dataBundle,
       _regex: /\{\{(\w+)\}\}/g,
       _wrapper: 'product_wrapper',
-      srcPrefix: 'https:',
     };
 
       this.initScraper();
   }
   initScraper(){
     const { data } = this.config;
-    
     const results = Promise.all(data.map(d => { 
-      return this.scrapeData(d);
-    }));
 
+      return Promise.all(d.url.map(url => { return this.scrapeData(d, url); }))
+
+    }));
+    
     results.then((data) => this.makeTemplate(data));
     results.then((data) => this.addData(data));
     
@@ -257,17 +71,18 @@ class Scraper {
     });
     
   }
-  scrapeData(obj) {
-    const request = rp(obj.url)
+  scrapeData(obj, test) {
+    const request = rp(test)
       .then((html) => {
         return {
-          title: $(obj.title, html).text(),
+          title: $(obj.data.title, html).text(),
           price: {
-            old: $(obj.price.old, html).text(),
-            new: $(obj.price.new, html).text(),
+            old: $(obj.data.price.old, html).text(),
+            new: $(obj.data.price.new, html).text(),
           },
-          image: $(obj.image, html).attr('src'),
-          link: obj.url,
+          image: $(obj.data.image, html).attr('src'),
+          link: test,
+          srcPrefix: obj.data.srcPrefix,
         };
       });
 
@@ -277,13 +92,16 @@ class Scraper {
   makeTemplate(data) {
     const { template } = this.config;
     data.map(d => {
-      d.template = this.replacePlaceholder(template._string, {
-        title: d.title,
-        oldprice: d.price.old,
-        newprice: d.price.new,
-        image: d.image,
-        link: d.link
-      })
+      d.map(d => {
+        d.template = this.replacePlaceholder(template._string, {
+          title: d.title,
+          oldprice: d.price.old,
+          newprice: d.price.new,
+          image: d.image,
+          link: d.link,
+          srcprefix: d.srcPrefix,
+        });
+      });
     });
   }
 
@@ -297,8 +115,9 @@ class Scraper {
     const wrapper = document.getElementById(this.config._wrapper);
   
     data.map(d => {
-      console.log(d.template);
-      // wrapper.insertAdjacentHTML("beforeend",  d.template);
+      d.map(d => {
+        wrapper.insertAdjacentHTML("beforeend",  d.template);
+      });
     });
   }
 }
